8a2819d6735d0779e4afab0314d1215f29368c50
Gaoussou Doucoure <gaousd4@gmail.com>
2025-04-16 20:13:10 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.print("Enter movie name: ");
+        String movie = read.nextLine();
+        System.out.print("Enter row number: ");
+        int row = read.nextInt();
+        System.out.print("Enter seat number: ");
+        int seat = read.nextInt();
+        Ticket ticket = new Ticket(movie, row, seat);
+        System.out.println("Movie: " + ticket.getMovie());
+        System.out.println("Row: " + ticket.getRow());
+        System.out.println("Seat: " + ticket.getSeat());
+    }
+}
+
+class Ticket {
+    private String movie;
+    private int row;
+    private int seat;
+
+    //USING "this" keyboard if the variable of the instances are the same as the parameters of the functions, below I will do the same snippets of code without the "this" keyword and it will also work
+    public Ticket(String movie, int row, int seat) {
+        this.movie = movie;
+        this.row = row;
+        this.seat = seat;
+    }
+    //WITHOUT USING "this" keyword
+/*    public Ticket(String film, int rang, int chaise) {
+        movie = film;
+        row = rang;
+        seat = chaise;
+    }*/
+    public String getMovie() {
+        return movie;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getSeat() {
+        return seat;
     }
 }
